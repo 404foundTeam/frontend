998d587b609b5d58bbd8fab31e4452e7d65eb2cb
jjong0923 <90667780+jjong0923@users.noreply.github.com>
2025-08-09 01:05:43 +0900

    add Header.jsx - jong

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,18 @@
+import logoImg from "../assets/logo.png";
+import styles from "../styles/Header.module.css";
+
 function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <img src={logoImg} className={styles.logoImg}></img>
+      <div className={styles.tags}>
+        <a className={styles.text}>market BEE</a>
+        <a>홍보</a>
+        <a>스마트 리포트</a>
+        <a className={styles.my}>MY</a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
