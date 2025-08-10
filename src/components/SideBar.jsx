b747abae7a16fde29b67702ab24d1e9d31f4c80e
jjong0923 <90667780+jjong0923@users.noreply.github.com>
2025-08-10 22:24:51 +0900

    3

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,20 +1,20 @@
 import styles from "../styles/SideBar.module.css";
-import main from "../assets/sidebar/main.png";
-import picture from "../assets/sidebar/picture.png";
-import carnews from "../assets/sidebar/carnews.png";
-import map from "../assets/sidebar/map.png";
+import main from "../assets/sidebar/main.ico";
+import camera from "../assets/sidebar/camera.ico";
+import carnews from "../assets/sidebar/cardnews.ico";
+import map from "../assets/sidebar/map.ico";
 
 function SideBar() {
-  const show = true;
+  const show = false;
 
   return (
     <div className={styles.container}>
-      <div className={`${styles.box} ${show ? styles.select : ""}`}>
+      <div className={`${styles.box} ${styles.select}`}>
         <img src={main} className={styles.main} />
         <p className={styles.text}>메인페이지</p>
       </div>
       <div className={`${styles.box} ${show ? styles.select : ""}`}>
-        <img src={picture} className={styles.picture} />
+        <img src={camera} className={styles.picture} />
         <p className={styles.text}>사진 가이드</p>
       </div>
       <div className={`${styles.box} ${show ? styles.select : ""}`}>
