a72ace7596c855bcefb955ed03881efd25981271
jjong0923 <90667780+jjong0923@users.noreply.github.com>
2025-08-12 00:09:57 +0900

    카카오 맵 추가....

--- src/components/WelcomoMap.jsx
+++ src/components/WelcomoMap.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import close from "../assets/welcomeMap/map_close.png";
 import listTitle from "../assets/welcomeMap/list_title.png";
 import listIcon from "../assets/welcomeMap/list_icon.png";
 import "../styles/WelcomeMap.css";
 import StoreSearch from "./StoreSearch";
+// import { Map } from "react-kakao-maps-sdk";
+// npm install react-kakao-maps-sdk
+
+const { kakao } = window;
 
 function WelcomeMap({ focusRef, onClick }) {
   const [search, setSearch] = useState("");
+  const container = useRef(null);
+
+  useEffect(() => {
+    const position = new kakao.maps.LatLng(35.114513, 129.039346);
+    const options = {
+      center: position,
+      level: 3,
+    };
+
+    new kakao.maps.Map(container.current, options);
+  }, []);
 
   const onChange = (e) => {
     setSearch(e.target.value);
@@ -33,7 +48,13 @@ function WelcomeMap({ focusRef, onClick }) {
         </div>
       </div>
       <div className="map-box">
-        <div className="map"></div>
+        <div className="map" ref={container}>
+          {/* <Map
+            center={{ lat: 33.450701, lng: 126.570667 }}
+            style={{ width: "1000px", height: "600px" }}
+            level={3}
+          /> */}
+        </div>
         <div className="search-lists">
           <div className="search-list-title">
             업장 검색 결과
@@ -45,39 +66,32 @@ function WelcomeMap({ focusRef, onClick }) {
               <div className="store-info">
                 <h1>모퉁이 꽃집</h1>
                 <p>
-                  경기도 용인시 기흥구 기흥역로58번길 78 기흥역더샵오피스텔
-                  201동 112호
+                  경기 용인시 기흥구 기흥역로58번길 78 기흥역더샵오피스텔 201동
+                  112호
                 </p>
               </div>
             </div>
             <div className="search-list-box">
               <img src={listIcon} className="list-box-ico" />
               <div className="store-info">
-                <h1>모퉁이 꽃집</h1>
+                <h1>진 부엉이꼬마김밥 기흥점</h1>
                 <p>
-                  경기도 용인시 기흥구 기흥역로58번길 78 기흥역더샵오피스텔
-                  201동 112호
+                  경기 용인시 기흥구 기흥역로63 힐스테이트 상가 1층 302동 108호
                 </p>
               </div>
             </div>
             <div className="search-list-box select">
               <img src={listIcon} className="list-box-ico" />
               <div className="store-info">
-                <h1>모퉁이 꽃집</h1>
-                <p>
-                  경기도 용인시 기흥구 기흥역로58번길 78 기흥역더샵오피스텔
-                  201동 112호
-                </p>
+                <h1>어웨이 커피</h1>
+                <p>경기 용인시 기흥구 기흥역로 9 108호 어웨이커피</p>
               </div>
             </div>
             <div className="search-list-box">
               <img src={listIcon} className="list-box-ico" />
               <div className="store-info">
-                <h1>모퉁이 꽃집</h1>
-                <p>
-                  경기도 용인시 기흥구 기흥역로58번길 78 기흥역더샵오피스텔
-                  201동 112호
-                </p>
+                <h1>구갈대지서점</h1>
+                <p>경기 용인시 기흥구 구갈로72번길 10 낙원상가 108호</p>
               </div>
             </div>
           </div>
