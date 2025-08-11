8ca2931e40b76bfbb0e61e346fc182cc22d66276
jjong0923 <90667780+jjong0923@users.noreply.github.com>
2025-08-11 18:32:15 +0900

    1

--- src/pages/CardNewsPage.jsx
+++ src/pages/CardNewsPage.jsx
@@ -1,9 +1,17 @@
+import "../styles/CardNewsPage.css";
+
 import SideBar from "../components/SideBar";
+import bannerImg from "../assets/cardnews/banner_img.png";
 
 function CardNewsPage() {
   return (
     <>
       <SideBar />
+      <div className="cardnews-banner">
+        <img src={bannerImg} className="banner-img" />
+        <h1>어웨이 커피</h1>
+        <p>를 위한 SNS 카드 뉴스를 만들어드릴게요.</p>
+      </div>
     </>
   );
 }
