576fdad68f0372c2211de6c644fdf125b3f387e5
jjong0923 <90667780+jjong0923@users.noreply.github.com>
2025-08-17 12:15:42 +0900

    update uuid - jong

--- src/store/sotreUuid.js
+++ src/store/sotreUuid.js
@@ -1,8 +1,11 @@
 import { create } from "zustand";
 // uuid 전역 상태 관리
 const storeUuid = create((set) => ({
-  uuid: null,
-  setUuid: (uuid) => set({ uuid }),
+  storeUuid: null,
+  isNew: null,
+  storeName: null,
+  setUuid: ({ storeUuid, isNew, storeName }) =>
+    set({ storeUuid, isNew, storeName }),
 }));
 
 export default storeUuid;
