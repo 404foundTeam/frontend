daada708ab31c502b9ebf9d4ecc110245ef7cb20
jjong0923 <90667780+jjong0923@users.noreply.github.com>
2025-08-21 11:29:27 +0900

    update header,sidebar rotuing - jong

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,6 +7,7 @@ import cardnews from "../assets/sidebar/cardnews.png";
 import cardnewsSelect from "../assets/sidebar/cardnews_select.png";
 import map from "../assets/sidebar/map.png";
 import mapSelect from "../assets/sidebar/map_select.png";
+import { NavLink } from "react-router-dom";
 
 // svg는 나중에 하는 걸로...
 // import { ReactComponent as MainIcon } from "../assets/sidebar/main.svg";
@@ -23,21 +24,30 @@ function SideBar({
         <img src={isMain ? mainSelect : main} className={styles.main} />
         <p className={styles.text}>메인페이지</p>
       </div>
-      <div className={`${styles.box} ${isCamera ? styles.select : ""}`}>
-        <img src={isCamera ? cameraSelect : camera} className={styles.camera} />
-        <p className={styles.text}>사진 가이드</p>
-      </div>
-      <div className={`${styles.box} ${isCard ? styles.select : ""}`}>
-        <img
-          src={isCard ? cardnewsSelect : cardnews}
-          className={styles.cardnews}
-        />
-        <p className={styles.text}>카드 뉴스 생성</p>
-      </div>
-      <div className={`${styles.box} ${isMap ? styles.select : ""}`}>
-        <img src={isMap ? mapSelect : map} className={styles.map} />
-        <p className={styles.text}>지도 이용 제휴</p>
-      </div>
+      <NavLink to="/camera">
+        <div className={`${styles.box} ${isCamera ? styles.select : ""}`}>
+          <img
+            src={isCamera ? cameraSelect : camera}
+            className={styles.camera}
+          />
+          <p className={styles.text}>사진 가이드</p>
+        </div>
+      </NavLink>
+      <NavLink to="/cardnews">
+        <div className={`${styles.box} ${isCard ? styles.select : ""}`}>
+          <img
+            src={isCard ? cardnewsSelect : cardnews}
+            className={styles.cardnews}
+          />
+          <p className={styles.text}>카드 뉴스 생성</p>
+        </div>
+      </NavLink>
+      <NavLink to="/map">
+        <div className={`${styles.box} ${isMap ? styles.select : ""}`}>
+          <img src={isMap ? mapSelect : map} className={styles.map} />
+          <p className={styles.text}>지도 이용 제휴</p>
+        </div>
+      </NavLink>
     </div>
   );
 }
