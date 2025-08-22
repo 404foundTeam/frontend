b59df3e5a86449ca1f555671d0bbe201674f6ad9
jjong0923 <90667780+jjong0923@users.noreply.github.com>
2025-08-22 13:33:00 +0900

    header,footer, welcomePage - jong

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,14 @@ import styles from "../styles/Header.module.css";
 
 function Header() {
   return (
-    <header className={styles.header}>
+    <header>
       <img src={logoImg} className={styles.img}></img>
       <div className={styles.tags}>
         <Link className={styles.text}>market BEE</Link>
         <NavLink className={styles.link}>홍보</NavLink>
         <NavLink className={styles.link}>스마트 리포트</NavLink>
-        <NavLink to="/mypage" className={`${styles.my} ${styles.link}`}>
-          MY
+        <NavLink to="/mypage" className={styles.link}>
+          마이페이지
         </NavLink>
       </div>
     </header>
--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -81,34 +81,37 @@ function WelcomePage() {
       {showMap && <WelcomeMap focusRef={mapRef} onClick={clickHideMap} />}
       <div className="welcome-header-container">
         <div className="welcome-header-box">
-          <div>
-            <h1 className="header-title">
-              소상공인을 위한
-              <br />
-              스마트한 마케팅 플랫폼
-            </h1>
-            <p className="header text">
-              어렵고 복잡한 마케팅과 운영전략을 한번에
-            </p>
-          </div>
+          <h1 className="header-title">
+            소상공인을 위한
+            <br />
+            스마트한 마케팅 플랫폼
+          </h1>
+          <p className="header text">
+            어렵고 복잡한 마케팅과 운영전략을 한번에
+          </p>
           <div className="header-img bee"></div>
           <div className="header-img bee"></div>
           <div className="header-img hive"></div>
         </div>
         <div className="welcome-header-bottom-box"></div>
       </div>
-      <TextBox />
+      <TextBox
+        title={"소상공인을 위한 쉽고 빠른 플랫폼, market BEE"}
+        content={
+          "클릭 몇 번으로 가게 홍보에 필요한 모든 콘텐츠와 운영을 자동으로 관리해보세요."
+        }
+      />
       <div className="welcome-card-wrapper">
         <div
           className="welcome-card report"
           ref={(el) => (cardRefs.current[0] = el)}
         >
-          <div className="line-top"></div>
-          <div className="welcome-text-box right">
+          <div className="line"></div>
+          <div className="welcome-text-box report">
             <p className="mini-title">업장 운영 전략</p>
             <h1 className="card-title">스마트 리포트 생성</h1>
             <p className="card-content">
-              AI가 리뷰, 방문 데이터를 분석해 매출이 오르는 패턴을 찾아드립니다.
+              AI가 리뷰, 방문 데이터를 분석해 매출이 오르는 패턴을 찾아드려요.
               <br />잘 팔리는 메뉴부터 개선 포인트까지 한 눈에 보이는 스마트
               리포트를 받아보세요.
             </p>
@@ -127,13 +130,13 @@ function WelcomePage() {
               </div>
             ))}
           </div>
-          <div className="welcome-text-box left">
+          <div className="welcome-text-box marketing">
             <p className="mini-title">업장 홍보</p>
             <h1 className="card-title">AI 마케팅</h1>
             <p className="card-content">
               마케팅 방식을 잘 모르는 사람을 위해서
               <br />
-              AI가 분석한 데이터를 바탕으로 마케팅 방안을 제시합니다.
+              AI가 분석한 데이터를 바탕으로 마케팅 방안을 알려줘요.
             </p>
           </div>
         </div>
@@ -141,25 +144,28 @@ function WelcomePage() {
           className="welcome-card sns"
           ref={(el) => (cardRefs.current[2] = el)}
         >
-          <div className="welcome-text-box right">
+          <div className="welcome-text-box sns">
             <p className="mini-title">업장 홍보</p>
             <h1 className="card-title">SNS 카드 생성</h1>
             <p className="card-content">
-              AI가 생성한 문구를 활용하여 카드 뉴스를 생성하세요.
+              AI가 생성한 문구를 활용하여 카드 뉴스를 생성해보세요.
               <br />
               디자인 전문가 없이 SNS 카드 뉴스를 클릭 몇 번으로 완성할 수
-              있습니다.
+              있어요.
             </p>
           </div>
-          <div className="cards-box right">
+          <div className="card-img-box">
             <img src={cardImg01} className="card-img" />
             <img src={cardImg02} className="card-img" />
             <img src={cardImg03} className="card-img" />
           </div>
-          <div className="line-bottom"></div>
+          <div className="line"></div>
         </div>
       </div>
-      <TextBox />
+      <TextBox
+        title={"가게 운영과 마케팅을 한 번에 잡는 스마트 솔루션"}
+        content={"사장님을 위한 새로운 성장과 전략, 지금 시작하세요."}
+      />
       <div className="welcome-sign-map">
         <h2 className="welcome-sign-title">업장 등록하기</h2>
         <p className="sign text">
