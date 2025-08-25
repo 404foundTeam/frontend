c0440524e7c7a8ae2b0558edbc6cea180e0cafd5
jjong0923 <90667780+jjong0923@users.noreply.github.com>
2025-08-25 13:52:24 +0900

    nnn차 스타일 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,10 @@
 // src/pages/MainPage.jsx
 
-import styles from '../styles/Main.module.css';
-import WeeklyScheduleViewer from '../components/main/WeeklyScheduleViewer';
-import MarketingTips from '../components/main/MarketingTips';
-import ReportSummary from '../components/main/ReportSummary'; // 1. ReportSummary 컴포넌트 import
-import DashboardPage from '../components/main/DashboardPage';
-
+import styles from "../styles/Main.module.css";
+import WeeklyScheduleViewer from "../components/main/WeeklyScheduleViewer";
+import MarketingTips from "../components/main/MarketingTips";
+import ReportSummary from "../components/main/ReportSummary"; // 1. ReportSummary 컴포넌트 import
+import DashboardPage from "../components/main/DashboardPage";
 
 function MainPage() {
   return (
